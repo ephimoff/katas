8f45f45299557b1d09e246544321c140077234bf
Anton Efimov <a.ephimoff@gmail.com>
2019-01-16 10:56:52 +1300

    Added unit tests

--- scripts/02.py
+++ scripts/02.py
@@ -1,1 +1,36 @@
+# Karate Chop
+import unittest
+
 print("Hello from Kata 02")
+
+def chop( x, array ):
+  return
+
+# Unit testing
+class ChopTestCase(unittest.TestCase):
+  # tests
+  def test_chop(self):
+    self.assertEqual(-1, chop(3, []))
+    self.assertEqual(-1, chop(3, [1]))
+    self.assertEqual(0,  chop(1, [1]))
+    #
+    self.assertEqual(0,  chop(1, [1, 3, 5]))
+    self.assertEqual(1,  chop(3, [1, 3, 5]))
+    self.assertEqual(2,  chop(5, [1, 3, 5]))
+    self.assertEqual(-1, chop(0, [1, 3, 5]))
+    self.assertEqual(-1, chop(2, [1, 3, 5]))
+    self.assertEqual(-1, chop(4, [1, 3, 5]))
+    self.assertEqual(-1, chop(6, [1, 3, 5]))
+    #
+    self.assertEqual(0,  chop(1, [1, 3, 5, 7]))
+    self.assertEqual(1,  chop(3, [1, 3, 5, 7]))
+    self.assertEqual(2,  chop(5, [1, 3, 5, 7]))
+    self.assertEqual(3,  chop(7, [1, 3, 5, 7]))
+    self.assertEqual(-1, chop(0, [1, 3, 5, 7]))
+    self.assertEqual(-1, chop(2, [1, 3, 5, 7]))
+    self.assertEqual(-1, chop(4, [1, 3, 5, 7]))
+    self.assertEqual(-1, chop(6, [1, 3, 5, 7]))
+    self.assertEqual(-1, chop(8, [1, 3, 5, 7]))
+
+if __name__ == '__main__':
+  unittest.main()
